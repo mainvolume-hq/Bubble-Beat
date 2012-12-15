cefe87fea0283f9dc33816fafdd0b96353b33b5e
Scott McCoid <mccoid.scott@gmail.com>
2012-12-15 09:39:26 -0800

    added audio session and audio unit setup code

--- Bubble Beat/BBAudioModel.h
+++ Bubble Beat/BBAudioModel.h
@@ -2,13 +2,27 @@
 //  BBAudioModel.h
 //  Bubble Beat
 //
-//  Created by Scott McCoid on 12/15/12.
+//  author: scott
+//  [sonic apps union]
 //
-//
+//  description: This is a static/singleton class that deals with
+//  all the audio processing in the application
 
 #import <Foundation/Foundation.h>
 #import <AudioToolbox/AudioToolbox.h>
 
 @interface BBAudioModel : NSObject
+{
+    AudioUnit bbUnit;                   // This is the actual audio unit instance variable
+    
+    int       blockSize;
+    int       sampleRate;
+}
+
++ (BBAudioModel *)sharedAudioModel;
+- (void)setupAudioUnit;
+- (void)setupAudioSession;
+
+// TODO: make properties for set-able values (blocksize, sr, etc...)
 
 @end
--- Bubble Beat/BBAudioModel.m
+++ Bubble Beat/BBAudioModel.m
@@ -2,9 +2,11 @@
 //  BBAudioModel.m
 //  Bubble Beat
 //
-//  Created by Scott McCoid on 12/15/12.
+//  author: scott
+//  [sonic apps union]
 //
-//
+//  description: This is a static/singleton class that deals with
+//  all the audio processing in the application. 
 
 #import "BBAudioModel.h"
 
@@ -45,6 +47,91 @@ static OSStatus renderCallback(void *inRefCon,
     }
     
     return self;
+}
+
+#pragma mark - Audio Unit Setup -
+
+- (void)setupAudioUnit
+{
+    AudioComponentDescription defaultOutputDescription;
+    defaultOutputDescription.componentType = kAudioUnitType_Output;
+    defaultOutputDescription.componentSubType = kAudioUnitSubType_RemoteIO;
+    defaultOutputDescription.componentManufacturer = kAudioUnitManufacturer_Apple;
+    defaultOutputDescription.componentFlags = 0;
+    defaultOutputDescription.componentFlagsMask = 0;
+    
+    // Find and assign default output unit
+    AudioComponent defaultOutput = AudioComponentFindNext(NULL, &defaultOutputDescription);
+    NSAssert(defaultOutput, @"-- Can't find a default output. --");
+    
+    // Create new audio unit that we'll use for output
+    OSErr err = AudioComponentInstanceNew(defaultOutput, &bbUnit);
+    NSAssert1(bbUnit, @"Error creating unit: %hd", err);
+    
+    // Enable IO for playback
+    UInt32 flag = 1;
+    err = AudioUnitSetProperty(bbUnit, kAudioOutputUnitProperty_EnableIO, kAudioUnitScope_Output, 0, &flag, sizeof(flag));
+    NSAssert1(err == noErr, @"Error setting output IO", err);
+    
+    // set format to 32 bit, single channel, floating point, linear PCM
+    const int fourBytesPerFloat = 4;
+    const int eightBitsPerByte = 8;
+    
+    AudioStreamBasicDescription streamFormat;
+    streamFormat.mSampleRate =       44100;
+    streamFormat.mFormatID =         kAudioFormatLinearPCM;
+    streamFormat.mFormatFlags =      kAudioFormatFlagsNativeFloatPacked | kAudioFormatFlagIsNonInterleaved;
+    streamFormat.mBytesPerPacket =   fourBytesPerFloat;
+    streamFormat.mFramesPerPacket =  1;
+    streamFormat.mBytesPerFrame =    fourBytesPerFloat;
+    streamFormat.mChannelsPerFrame = 1;
+    streamFormat.mBitsPerChannel =   fourBytesPerFloat * eightBitsPerByte;
+    
+    err = AudioUnitSetProperty(bbUnit, kAudioUnitProperty_StreamFormat, kAudioUnitScope_Input, 0, &streamFormat, sizeof(AudioStreamBasicDescription));
+    NSAssert1(err == noErr, @"Error setting stream format: %hd", err);
+    
+    // Output
+    // Setup rendering function on the unit
+    AURenderCallbackStruct input;
+    input.inputProc = renderCallback;
+    input.inputProcRefCon = (__bridge void *)self;
+    
+    // This sets the audio unit render callback
+    err = AudioUnitSetProperty(bbUnit, kAudioUnitProperty_SetRenderCallback, kAudioUnitScope_Global, 0, &input, sizeof(input));
+    NSAssert1(err == noErr, @"Error setting callback: %hd", err);
+    
+}
+
+- (void)setupAudioSession
+{
+    OSStatus status;
+    Float32 bufferDuration = (blockSize + 0.5) / sampleRate;           // add 0.5 to blockSize, need to so bufferDuration is correct value
+    UInt32 category = kAudioSessionCategory_MediaPlayback;
+    
+    status = AudioSessionInitialize(NULL, NULL, NULL, (__bridge void *)self);
+    status = AudioSessionSetProperty(kAudioSessionProperty_AudioCategory, sizeof(category), &category);
+    status = AudioSessionSetProperty(kAudioSessionProperty_PreferredHardwareSampleRate, sizeof(sampleRate), &sampleRate);
+    status = AudioSessionSetProperty(kAudioSessionProperty_PreferredHardwareIOBufferDuration, sizeof(bufferDuration), &bufferDuration);
+    
+    // TODO: Check where this should be set-able
+    status = AudioSessionSetActive(true);
+    
+    //--------- Check everything
+    Float64 audioSessionProperty64 = 0;
+    Float32 audioSessionProperty32 = 0;
+    UInt32 audioSessionPropertySize64 = sizeof(audioSessionProperty64);
+    UInt32 audioSessionPropertySize32 = sizeof(audioSessionProperty32);
+    
+    status = AudioSessionGetProperty(kAudioSessionProperty_CurrentHardwareSampleRate, &audioSessionPropertySize64, &audioSessionProperty64);
+    NSLog(@"AudioSession === CurrentHardwareSampleRate: %.0fHz", audioSessionProperty64);
+    
+    sampleRate = audioSessionProperty64;
+    
+    status = AudioSessionGetProperty(kAudioSessionProperty_CurrentHardwareIOBufferDuration, &audioSessionPropertySize32, &audioSessionProperty32);
+    int blockSizeCheck = lrint(audioSessionProperty32 * audioSessionProperty64);
+    NSLog(@"AudioSession === CurrentHardwareIOBufferDuration: %3.2fms", audioSessionProperty32 * 1000.0f);
+    NSLog(@"AudioSession === block size: %i", blockSizeCheck);
+    
 }
 
 @end
